eeb914de74b87997cdce3b799695beee334586a7
Anamika Rijal <anamika.rijal.13@gmail.com>
2024-05-28 02:30:57 +0545

    Update page.js
    Edited the Root Page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Hello World!</h1>
+    <main className="h-screen">
+      <h1 className="text-xl">CPRG 306: Web Development 2 - Assignments</h1>
+      <Link href="http://localhost:3000/week-2">Week 2 Assignment</Link>
     </main>
   );
 }
+
