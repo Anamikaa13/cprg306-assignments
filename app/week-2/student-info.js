d3f05a0742b748d5d2acbc1c3f3a056bc19c03f9
Anamika Rijal <anamika.rijal.13@gmail.com>
2024-05-31 12:52:37 +0545

    Update student-info.js

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 
 export default function Home() {
-    return (
-      <div>
-        <p>Anamika Rijal</p>
-        <Link href="https://github.com/Anamikaa13/cprg306-assignments">https://github.com</Link>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <p>Anamika Rijal</p>
+      <Link href="https://github.com/Anamikaa13/cprg306-assignments">https://github.com</Link>
+    </div>
+  );
+}
