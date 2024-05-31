9e1a285d26ec7107af734cea36820d19a290f673
Anamika Rijal <anamika.rijal.13@gmail.com>
2024-05-31 12:52:43 +0545

    Update item.js

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,15 +1,14 @@
 
 
 
-function Item ({ name, quantity, category }) {
+function Item({ name, quantity, category }) {
     return (
-      <li className="flex flex-col p-1 bg-gray-900 text-white rectangle-lg">
-        <span className="font-semibold text-xl">{name}</span>
-        <span className="text-white-400 text-l">Buy {quantity} in {category}</span>
-      </li>
+        <li className="flex flex-col p-1 bg-gray-900 text-white rectangle-lg">
+            <span className="font-semibold text-xl">{name}</span>
+            <span className="text-white-400 text-l">Buy {quantity} in {category}</span>
+        </li>
     );
-  }
-  
-  export default Item;
-  
-  
+}
+
+export default Item;
+
