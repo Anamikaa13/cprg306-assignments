d5d140215c09417a29864f9115a1e4333c59caed
Anamika Rijal <anamika.rijal.13@gmail.com>
2024-05-31 12:41:57 +0545

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,9 +2,18 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="h-screen">
-      <h1 className="text-xl">CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href="http://localhost:3000/week-2">Week 2 Assignment</Link>
+    <main className="p-4 bg-black min-h-screen flex justify-center text-xl text-white">
+      <div>
+      <h1 className="font-bold text-4xl mb-3">CPRG 306: Web Development 2 - Assignments</h1>
+      <ul>
+        <li>
+        <Link href="http://localhost:3000/week-2">Week 2 Assignment</Link>
+        </li>
+        <li>
+        <Link href="http://localhost:3000/week-3">Week 3 Assignment</Link>
+        </li>
+      </ul>
+      </div>
     </main>
   );
 }
