eedaf81573146045fc42a3dcf10ec88ebf80b2f9
Anamika Rijal <anamika.rijal.13@gmail.com>
2024-05-31 12:41:54 +0545

    Update page.js

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -2,7 +2,7 @@ import StudentInfo from './student-info';
 
 export default function Page() {
     return (
-      <main>
+      <main className='p-4 bg-black h-screen text-xl text-white'>
         <h1>Shopping List</h1>
         <StudentInfo/>
       </main>
