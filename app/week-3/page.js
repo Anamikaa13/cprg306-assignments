702cbe3aa753dde3e45c9c302874a5f4d5ec9191
Anamika Rijal <anamika.rijal.13@gmail.com>
2024-05-31 12:41:44 +0545

    Create page.js

---
+++ app/week-3/page.js
@@ -0,0 +1,14 @@
+import ItemList from './item-list';
+
+function Page() {
+    return (
+        <main className="p-4 bg-black min-h-screen flex justify-left">
+            <div className="w-full max-w-md">
+              <h1 className="text-3xl font-bold mb-3 text-white text-left">Shopping List</h1>
+        <ItemList />
+             </div>
+         </main>
+    );
+}
+
+export default Page;
