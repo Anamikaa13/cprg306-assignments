7cc959028fc3380b4466129b56d902891948823e
Anamika Rijal <anamika.rijal.13@gmail.com>
2024-05-31 12:52:40 +0545

    Update page.js

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -4,10 +4,10 @@ function Page() {
     return (
         <main className="p-4 bg-black min-h-screen flex justify-left">
             <div className="w-full max-w-md">
-              <h1 className="text-3xl font-bold mb-3 text-white text-left">Shopping List</h1>
-        <ItemList />
-             </div>
-         </main>
+                <h1 className="text-3xl font-bold mb-3 text-white text-left">Shopping List</h1>
+                <ItemList />
+            </div>
+        </main>
     );
 }
 
